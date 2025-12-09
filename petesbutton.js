6e85fda204cae87fbdf01749456d98d91f6bea31
Michaelt138 <Trout.Michael@student.greenriver.edu>
2025-12-09 10:35:37 -0800

    Pushing!

--- petesbutton.js
+++ petesbutton.js
@@ -1,6 +1,11 @@
 console.log("Football Genius");
 
-function logpress() {
-    console.log("Football Genius")
-    
+function pete () {
+    console.log("WoooHoo!");
+    document.body.innerHTML = '<img src="Footge.png" alt="Pete Carroll with lazer eyes">';
+   
+}
+
+function doGet() {
+  return HtmlService.createHtmlOutputFromFile('index');
 }
